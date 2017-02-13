8b932808514f9fda4aa9f82f29a1a29613a608ac
achaniotaki <achaniotaki@dmst.aueb.gr>
2017-02-13 11:55:27 +0200

    Added visitor related to fields

---
+++ src/FieldVisitor.java
@@ -0,0 +1,27 @@
+package project.metrics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+
+public class FieldVisitor extends ASTVisitor {
+	List<FieldDeclaration> fields = new ArrayList<FieldDeclaration>();
+
+	@Override
+	public boolean visit(FieldDeclaration field) {
+		fields.add(field);
+
+		return super.visit(field);
+	}
+
+	public List<FieldDeclaration> getFields() {
+		return fields;
+	}
+
+	public int countFields() {
+		return fields.size();
+	}
+
+}
