096ff7386228e72199f8aea7d84de93edcc4b5e2
achaniotaki <achaniotaki@dmst.aueb.gr>
2017-02-16 13:12:06 +0200

    restructured

--- src/Designite/SourceModel/ClassMetrics.java
+++ src/Designite/SourceModel/ClassMetrics.java
@@ -1,14 +1,6 @@
 package Designite.SourceModel;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Modifier;
-import org.eclipse.jface.text.Document;
 
 public class ClassMetrics {
 
@@ -16,12 +8,8 @@ public class ClassMetrics {
 	private int publicMethods = 0;
 	private int countFields = 0;
 	private int publicFields = 0;
-	private String pathToAnalyze;
 	//private int properties = 0;
-
-	public ClassMetrics(String pathToAnalyze){
-		this.pathToAnalyze = pathToAnalyze;
-	}
+	
 	public int getNoMethods() {
 		return countMethods;
 	}
@@ -44,38 +32,6 @@ public class ClassMetrics {
 	
 	public int getProperties() {
 		return countMethods + countFields;
-	}
-
-	public String readFileToString() {
-		try {
-			return new String(Files.readAllBytes(Paths.get(pathToAnalyze)));
-		} catch (IOException e) {
-			e.printStackTrace();
-			return new String();
-		}
-	}
-	
-
-	public CompilationUnit createAST(final String content) {
-		Document doc = new Document(content);
-		final ASTParser parser = ASTParser.newParser(AST.JLS8);
-		parser.setKind(ASTParser.K_COMPILATION_UNIT);
-		parser.setSource(doc.get().toCharArray());
-		parser.setResolveBindings(true);
-		CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-		return cu;
-	}
-
-	public void visitAST(CompilationUnit cu) {
-		MethodVisitor methodVisitor = new MethodVisitor();
-		cu.accept(methodVisitor);
-		computeMetrics(methodVisitor);
-
-		FieldVisitor fieldVisitor = new FieldVisitor();
-		cu.accept(fieldVisitor);
-		computeMetrics(fieldVisitor);
-		
-		printMetrics();
 	}
 
 	public void computeMetrics(MethodVisitor visitor) {
