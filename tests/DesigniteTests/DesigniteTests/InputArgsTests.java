b2bf8e07fbe710d8608e3dcac9c6c13468cfb158
Tushar <tushar@aueb.gr>
2017-02-18 22:45:01 +0200

    added tests for InputArgs

---
+++ tests/DesigniteTests/DesigniteTests/InputArgsTests.java
@@ -0,0 +1,36 @@
+package DesigniteTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Designite.InputArgs;
+
+public class InputArgsTests {
+	//Set this path before executing tests
+	private static String TESTS_PATH = "/Users/Tushar/Documents/Workspace/DesigniteJava/tests";
+	
+	//Negative case- folder path specified rather than input batch file
+	@Test(expected = IllegalArgumentException.class)
+	public void testInputArgs_negative_folder() {
+		new InputArgs(TESTS_PATH);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testInputArgs_negative_invalidPath() {
+		new InputArgs(TESTS_PATH+"/invalidFile.txt");
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testInputArgs_negative_invalidContents() {
+		new InputArgs(TESTS_PATH+"TestFiles/invalidBatchFile.txt");
+	}
+	@Test
+	public void testInputArgs_sourceFolder() {
+		InputArgs args = new InputArgs(TESTS_PATH + "/TestFiles/inBatchFile.txt");
+		assertEquals("/Users/Tushar/Documents/Workspace/DesigniteJava/src/", args.getSourceFolder());
+	}
+	@Test
+	public void testInputArgs_outputFolder() {
+		InputArgs args = new InputArgs(TESTS_PATH + "/TestFiles/inBatchFile.txt");
+		assertEquals("/Users/Tushar/Documents/Workspace/DesigniteJava/tests/temp/", args.getOutputFolder());
+	}
+}
