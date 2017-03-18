25fffe19832b3a8d042d8c2f2a160f0f0c124d40
achaniotaki <achaniotaki@dmst.aueb.gr>
2017-03-18 09:43:29 +0200

    set environment to resolve bindings

--- src/Designite/SourceModel/SM_Project.java
+++ src/Designite/SourceModel/SM_Project.java
@@ -6,7 +6,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import org.eclipse.jface.text.Document;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
@@ -19,12 +22,19 @@ public class SM_Project extends SM_SourceItem{
 	private List<String> sourceFileList;
 	private List<CompilationUnit> compilationUnitList;
 	private List<SM_Package> packageList;
+	private String unitName;
 	
 	public SM_Project(InputArgs argsObj) {
 		this.inputArgs = argsObj;
 		sourceFileList = new ArrayList<String>();
 		compilationUnitList = new ArrayList<CompilationUnit>();
 		packageList = new ArrayList<SM_Package>();
+		//setName();
+	}
+	
+	//TODO 
+	public void setName() {
+		
 	}
 	
 	public void setSourceFileList(List<String> list) {
@@ -60,7 +70,10 @@ public class SM_Project extends SM_SourceItem{
 	//method used in tests
 	public CompilationUnit createCU(String filePath) {
 		String fileToString = readFileToString(filePath);
-		return createAST(fileToString);
+		int startingIndex = filePath.lastIndexOf(File.separatorChar);
+		unitName = filePath.substring(startingIndex + 1);
+		
+		return createAST(fileToString, unitName);
 	}
 
 	private void parseAllPackages() {
@@ -113,21 +126,51 @@ public class SM_Project extends SM_SourceItem{
 		
 			for(String file: sourceFileList) {
 				String fileToString = readFileToString(file);
-				CompilationUnit unit = createAST(fileToString);
-					compilationUnitList.add(unit);
+				int startingIndex = file.lastIndexOf(File.separatorChar);
+				unitName = file.substring(startingIndex + 1);
+				CompilationUnit unit = createAST(fileToString, unitName);
+				compilationUnitList.add(unit);
 			} 
 		}catch(NullPointerException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private CompilationUnit createAST(final String content) {
+	//set sources and classpath before execute
+	private CompilationUnit createAST(final String content, String unitName) {
  		Document doc = new Document(content);
  		final ASTParser parser = ASTParser.newParser(AST.JLS8);
- 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
  		parser.setSource(doc.get().toCharArray());
+ 		parser.setKind(ASTParser.K_COMPILATION_UNIT); 
+ 		
+		//parser.setUnitName(unitName);
+ 		
+/*		Map<String, String> options = JavaCore.getOptions();
+		parser.setCompilerOptions(options);*/
+		
+		String[] sources = { "C:\\Users\\Alex\\workspace\\DesisgniteJava" }; 
+		String[] classpath = {"C:\\Users\\Alex\\workspace\\DesisgniteJava\\lib"};
+/*		String[] classpath = {"C:\\Program Files\\Java\\jre1.8.0_121\\lib\\rt.jar" };
+		String[] classpath = {"C:\\Users\\Alex\\workspace\\DesisgniteJava\\lib\\org.eclipse.core.contenttype_3.5.100.v20160418-1621.jar",
+				"C:\\Users\\Alex\\workspace\\DesisgniteJava\\lib\\org.eclipse.core.jobs_3.8.0.v20160509-0411.jar",
+				"C:\\Users\\Alex\\workspace\\DesisgniteJava\\lib\\org.eclipse.core.resources_3.11.1.v20161107-2032.jar",
+				"C:\\Users\\Alex\\workspace\\DesisgniteJava\\lib\\org.eclipse.core.runtime_3.12.0.v20160606-1342.jar",
+				"C:\\Users\\Alex\\workspace\\DesisgniteJava\\lib\\org.eclipse.equinox.common_3.8.0.v20160509-1230.jar",
+				"C:\\Users\\Alex\\workspace\\DesisgniteJava\\lib\\org.eclipse.equinox.preferences_3.6.1.v20160815-1406.jar",
+				"C:\\Users\\Alex\\workspace\\DesisgniteJava\\lib\\org.eclipse.jdt.core-3.10.0.jar",
+				"C:\\Users\\Alex\\workspace\\DesisgniteJava\\lib\\org.eclipse.osgi_3.11.2.v20161107-1947.jar",
+				"C:\\Users\\Alex\\workspace\\DesisgniteJava\\lib\\org.eclipse.text_3.5.0.jar"};*/
+
+ 		
+ 		parser.setEnvironment(classpath, sources, new String[] {"UTF-8"}, true);
  		parser.setResolveBindings(true);
+ 		parser.setBindingsRecovery(true);
+ 	    parser.setStatementsRecovery(true);
  		CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+ 		
+ 		if (cu.getAST().hasBindingsRecovery()) {
+			System.out.println("Binding activated.");
+		}
  		return cu;
  	}
 	
