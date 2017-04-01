bc4652d7547f618343f145b8f66d99afacc45e2f
achaniotaki <achaniotaki@dmst.aueb.gr>
2017-04-01 21:35:07 +0300

    New visitor for local variables

---
+++ src/Designite/SourceModel/LocalVarVisitor.java
@@ -0,0 +1,37 @@
+package Designite.SourceModel;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
+
+public class LocalVarVisitor extends ASTVisitor {
+	List<SM_LocalVar> localVariables = new ArrayList<SM_LocalVar>();
+	private MethodDeclaration methodDeclaration;
+	private TypeDeclaration typeDeclaration;
+	
+	public boolean visit(VariableDeclarationStatement variable){
+		for (Iterator iter = variable.fragments().iterator(); iter.hasNext();) {
+			VariableDeclarationFragment fragment = (VariableDeclarationFragment) iter.next();
+//			IVariableBinding binding = fragment.resolveBinding();
+			
+			SM_LocalVar newLocalVar = new SM_LocalVar(fragment);
+			newLocalVar.setType(variable.getType());
+			localVariables.add(newLocalVar);
+		}
+		return super.visit(variable);
+	}
+	
+	public List<SM_LocalVar> getLocalVar() {
+		return localVariables;
+	}
+
+	public int countParameters() {
+		return localVariables.size();
+	}
+}
